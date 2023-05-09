a1392db8fafcdd5f116b0b0a84e9d3787eb5fff6
vinay <vinaypallerla5664@gmail.com>
2023-05-09 14:15:42 +0000

    Routing using React Router

---
+++ src/components/Header/index.js
@@ -0,0 +1,23 @@
+import './index.css'
+import {Link} from 'react-router-dom'
+
+const Header = () => (
+  <nav className="nav-header">
+    <div className="blog-container">
+      <h1 className="blog-title">My Blog</h1>
+      <ul className="nav-menu">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="about">About</Link>
+        </li>
+        <li>
+          <Link to="contact">Contact</Link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+)
+
+export default Header
